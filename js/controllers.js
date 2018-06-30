b2e5ffb5e313bd5703aa93b53faab2547cc786bb
Aleksandr Sobolev <alsobol@mera.ru>
2018-06-30 15:59:52 +0300

    add location and route

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,52 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope, $http) {
-    $http.get('phones/phones.json').success(function(data, status, headers, config) {
-        console.log('This is Data:',data,'\n\nThis is Status:',status,'\n\nThis is Headers:',headers,'\n\nThis is config:',config);
-        $scope.phones = data;
-    });
+phonecatApp.config(['$routeProvider', '$locationProvider', function($routeProvide, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  })
+  $routeProvide
+    .when('/', {
+      templateUrl: 'template/home.html',
+      controller: 'PhoneListCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'template/about.html',
+      controller: 'AboutCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'template/contact.html',
+      controller: 'ContactCtrl'
+    })
+    .when('/phones/:phoneId', {
+      templateUrl: 'template/phone-detail.html',
+      controller: 'PhoneDetailCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+}])
 
-    $scope.today = new Date();
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+
+  $http.get('phones/phones.json').success(function(data, status, headers, config) {
+    $scope.phones = data;
+  });
+
+  $scope.today = new Date();
 }]);
 
+phonecatApp.controller('AboutCtrl', ['$scope','$http', '$location', function($scope, $http, $location) {
 
+}]);
 
+phonecatApp.controller('ContactCtrl', ['$scope','$http', '$location', function($scope, $http, $location) {
+
+}]);
+
+phonecatApp.controller('PhoneDetailCtrl', ['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+  $scope.phoneId = $routeParams.phoneId;
+}]);
